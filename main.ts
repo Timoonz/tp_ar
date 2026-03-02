f7416b0698f9ca72b45094fbb8600d10545f3e94
Timoonz <timotheeeheraud@gmail.com>
2026-03-02 17:01:57 +0100

    adapt conescript typescript

--- main.ts
+++ main.ts
@@ -19,162 +19,164 @@ import {
   MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
-  WebGLRenderer
+  WebGLRenderer,
+  RingGeometry,
+  MeshBasicMaterial,
+  Object3D,
+  Object3DEventMap
 } from 'three';
 
 // XR Emulator
-import { DevUI } from '@iwer/devui';
-import { XRDevice, metaQuest3 } from 'iwer';
 
-// XR
-import { XRButton } from 'three/addons/webxr/XRButton.js';
 
-// If you prefer to import the whole library, with the THREE prefix, use the following line instead:
-// import * as THREE from 'three'
-
-// NOTE: three/addons alias is supported by Rollup: you can use it interchangeably with three/examples/jsm/  
-
-// Importing Ammo can be tricky.
-// Vite supports webassembly: https://vitejs.dev/guide/features.html#webassembly
-// so in theory this should work:
-//
-// import ammoinit from 'three/addons/libs/ammo.wasm.js?init';
-// ammoinit().then((AmmoLib) => {
-//  Ammo = AmmoLib.exports.Ammo()
-// })
-//
-// But the Ammo lib bundled with the THREE js examples does not seem to export modules properly.
-// A solution is to treat this library as a standalone file and copy it using 'vite-plugin-static-copy'.
-// See vite.config.js
-// 
-// Consider using alternatives like Oimo or cannon-es
-import {
-  OrbitControls
-} from 'three/addons/controls/OrbitControls.js';
-
-import {
-  GLTFLoader
-} from 'three/addons/loaders/GLTFLoader.js';
-import { XRController } from 'iwer/lib/device/XRController';
+// AR
+import { ARButton } from 'three/addons/webxr/ARButton.js';
 
 // Example of hard link to official repo for data, if needed
 // const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r173/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
 
+let container;
 
+let camera: PerspectiveCamera, scene: Scene, renderer: WebGLRenderer;
 
+let reticle: Object3D<Object3DEventMap>;
 
-// INSERT CODE HERE
-let camera : PerspectiveCamera, scene : Scene, renderer : WebGLRenderer;
+let controller1, controller2;
 
+let hitTestSource: XRHitTestSource | null = null;
+let hitTestSourceRequested = false;
 
 const timer = new Timer();
 timer.connect(document);
 
 // Main loop
-const animate = () => {
 
-  timer.update();
-  const delta = timer.getDelta();
-  const elapsed = timer.getElapsed();
+function animate(_timestamp: any, frame: { getHitTestResults: (arg0: XRHitTestSource) => any; }) {
 
-  // can be used in shaders: uniforms.u_time.value = elapsed;
+  if (frame) {
+
+    const referenceSpace = renderer.xr.getReferenceSpace();
+    const session = renderer.xr.getSession();
+
+    if (hitTestSourceRequested === false) {
+      if (session) {
+        session.requestReferenceSpace('viewer').then(function (referenceSpace) {
+
+          session.requestHitTestSource?.({ space: referenceSpace })?.then(function (source) {
+
+            hitTestSource = source;
+
+          });
+
+        });
+
+        session.addEventListener('end', function () {
+
+          hitTestSourceRequested = false;
+          hitTestSource = null;
+
+        });
+
+        hitTestSourceRequested = true;
+      }
+
+
+    }
+
+    if (hitTestSource) {
+
+      const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+      if (hitTestResults.length) {
+
+        const hit = hitTestResults[0];
+
+        reticle.visible = true;
+        reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+
+      } else {
+
+        reticle.visible = false;
+
+      }
+
+    }
+
+  }
 
   renderer.render(scene, camera);
-};
+
+}
 
 
-const init = () => {
+
+function init() {
+
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
   scene = new Scene();
 
-  const aspect = window.innerWidth / window.innerHeight;
-  camera = new PerspectiveCamera(75, aspect, 0.1, 10); // meters
-  camera.position.set(0, 1.6, 3);
+  camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
-  const light = new AmbientLight(0xffffff, 1.0); // soft white light
+  const light = new HemisphereLight(0xffffff, 0xbbbbff, 3);
+  light.position.set(0.5, 1, 0.25);
   scene.add(light);
 
-  const hemiLight = new HemisphereLight(0xffffff, 0xbbbbff, 3);
-  hemiLight.position.set(0.5, 1, 0.25);
-  scene.add(hemiLight);
+  //
 
   renderer = new WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate); // requestAnimationFrame() replacement, compatible with XR 
+  renderer.setAnimationLoop(animate);
   renderer.xr.enabled = true;
-  document.body.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
 
-  /*
-  document.body.appendChild( XRButton.createButton( renderer, {
-    'optionalFeatures': [ 'depth-sensing' ],
-    'depthSensing': { 'usagePreference': [ 'gpu-optimized' ], 'dataFormatPreference': [] }
-  } ) );
-*/
+  //
 
-  const xrButton = XRButton.createButton(renderer, {});
-  xrButton.style.backgroundColor = 'skyblue';
-  document.body.appendChild(xrButton);
+  document.body.appendChild(ARButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  //controls.listenToKeyEvents(window); // optional
-  controls.target.set(0, 1.6, 0);
-  controls.update();
+  //
 
-  // Handle input: see THREE.js webxr_ar_cones
+  const geometry = new CylinderGeometry(0.1, 0.1, 0.2, 32).translate(0, 0.1, 0);
 
-  const geometry = new CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+  function onSelect() {
 
-  const onSelect = (event : any) => {
+    if (reticle.visible) {
 
-    const material = new MeshPhongMaterial({ color: 0xffffff * Math.random() });
-    const mesh = new Mesh(geometry, material);
-    mesh.position.set(0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
-    mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-    scene.add(mesh);
+      const material = new MeshPhongMaterial({ color: 0xffffff * Math.random() });
+      const mesh = new Mesh(geometry, material);
+      reticle.matrix.decompose(mesh.position, mesh.quaternion, mesh.scale);
+      mesh.scale.y = Math.random() * 2 + 1;
+      scene.add(mesh);
+
+    }
 
   }
 
-  const controller = renderer.xr.getController(0);
-  controller.addEventListener('select', onSelect);
-  scene.add(controller);
+  controller1 = renderer.xr.getController(0);
+  controller1.addEventListener('select', onSelect);
+  scene.add(controller1);
 
+  controller2 = renderer.xr.getController(1);
+  controller2.addEventListener('select', onSelect);
+  scene.add(controller2);
 
-  window.addEventListener('resize', onWindowResize, false);
+  reticle = new Mesh(
+    new RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2),
+    new MeshBasicMaterial()
+  );
+  reticle.matrixAutoUpdate = false;
+  reticle.visible = false;
+  scene.add(reticle);
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
 
 }
 
 init();
-
-//
-
-/*
-function loadData() {
-  new GLTFLoader()
-    .setPath('assets/models/')
-    .load('test.glb', gltfReader);
-}
-
-
-function gltfReader(gltf) {
-  let testModel = null;
-
-  testModel = gltf.scene;
-
-  if (testModel != null) {
-    console.log("Model loaded:  " + testModel);
-    scene.add(gltf.scene);
-  } else {
-    console.log("Load FAILED.  ");
-  }
-}
-
-loadData();
-*/
-
-
-// camera.position.z = 3;
-
-
 
 
 function onWindowResize() {
